be21a1db12cd4b086df2bf86ef2a9ecd31483fbb
Sean Conkie <55431380+sean-conkie@users.noreply.github.com>
2025-04-14 22:17:56 +0100

    Enhance SQLLineageParser with expression extraction and logging capabilities

--- src/sql2lineage/__init__.py
+++ src/sql2lineage/__init__.py
@@ -1,4 +1,15 @@
 """sql2lineage package."""
 
+from os import PathLike
+from pathlib import Path
+from typing import Optional, TypeAlias
+
 __app_name__ = "sql2lineage"
 __version__ = "0.2.0"
+
+StrPath: TypeAlias = str | PathLike[str]
+
+
+def walk_dir(path: StrPath, glob: Optional[str] = None):
+
+    pass
